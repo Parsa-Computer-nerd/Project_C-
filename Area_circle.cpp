b26f676f5241265520a5c764198f54ab1f1b06a9
Parsa-Computer-nerd <pourmollajamal.parsa@gmail.com>
2026-02-11 19:29:46 +0330

    add C++ project

---
+++ Area_circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int r;
+    float S;
+
+    cout << "Please enter the radius of the circle : ";
+    cin >> r;
+
+    S = 3.14 * r * r;
+    cout << "Area : " << S;
+}
