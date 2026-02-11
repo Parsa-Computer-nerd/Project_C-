5cb795dee6754b75fea54f2b5e147b97e2638527
Parsa-Computer-nerd <pourmollajamal.parsa@gmail.com>
2026-02-11 19:29:16 +0330

    add C++ project

---
+++ Average_two_number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int x, y;
+    float ave;
+
+    cout << "Enter numer one : ";
+    cin >> x;
+    cout << "Enter numer two : ";
+    cin >> y;
+
+    ave = (x + y) / 2.0;
+
+    cout << "Average : " << ave;
+}
